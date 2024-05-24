2b57fc5e8c70ab6f272600fff13d31e2c876abb6
Makhdum412 <75497174+Makhdum412@users.noreply.github.com>
2024-05-24 17:32:16 +0530

    Create trycatch.cpp

---
+++ ExceptionHandling/trycatch.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the values of a and b: ";
+    cin>>a>>b;
+    int x= a-b;
+    try{
+        if(x!=0){
+            cout<<"Result= "<<a/x<<endl;
+        }
+        else{
+            throw(x);
+        }
+    }
+    catch(int i){
+        cout<<"Exception caught: x="<<x<<endl;
+    }
+    cout<<"END";
+    return 0;
+}
